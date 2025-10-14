0ae1cbf1b88117fab35307581a30fceee1763fe2
Nerdy Gedoni <gedoniani@gmail.com>
2025-10-14 10:27:39 +0100

    feat: add health check  endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import envConfig from './config/env.config';
+import { PrismaService } from './config/prisma.service';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, load: [envConfig] })],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [envConfig],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class AppModule {}
--- src/config/prisma.service.ts
+++ src/config/prisma.service.ts
@@ -1,12 +1,37 @@
-// src/config/prisma.service.ts
-import { Injectable, OnModuleInit } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import {
+  Injectable,
+  OnModuleInit,
+  OnModuleDestroy,
+  Logger,
+} from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit(): Promise<void> {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    await this.$connect();
-    console.log('Connected to Supabase Postgres ✅');
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  private readonly logger = new Logger(PrismaService.name);
+
+  constructor() {
+    super({
+      log: ['query', 'error', 'warn'],
+    });
+  }
+
+  async onModuleInit() {
+    try {
+      await this.$connect();
+      this.logger.log('✅ Connected to Supabase PostgreSQL');
+    } catch (error) {
+      this.logger.error('❌ Failed to connect to database', error);
+      throw error;
+    }
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    this.logger.log('❌ Disconnected from Supabase PostgreSQL');
   }
 }
